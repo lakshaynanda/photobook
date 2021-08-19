64ed134127140ce1066182b166ebe4f258efc76f
lakshaynanda <lakshay.nanda08@gmail.com>
2021-08-19 00:43:45 -0700

    Authentication Complete with AWS Amplify

--- photobook-yt/src/App.vue
+++ photobook-yt/src/App.vue
@@ -1,13 +1,28 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> -->
+      <Nav></Nav>
     </div>
     <router-view/>
   </div>
 </template>
-
+<script>
+import Nav from '../src/components/Nav.vue'
+import {mapActions} from 'vuex'
+export default {
+  components: {
+    Nav
+  },
+  mounted() {
+    this.authAction()
+  },
+  methods: {
+    ...mapActions('auth', ['authAction'])
+  }
+}
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
